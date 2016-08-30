177cbf7507e2f98d693dc8542d3ea87c9ebc1ca3
Richard Hong <rich.hong@gmail.com>
2016-08-30 13:50:53 -0700

    fixing typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { createStore }      from 'redux';
 
 import exampleReducer from './public/src/reducers/root'
 
-import App from './public/src/components/app';
+import App from './public/src/components/appasdf';
 
 let store = createStore(exampleReducer);
 
