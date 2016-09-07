55809b06a0dfc4fd5321971b64efd8c23d4db31d
Richard Hong <rich.hong@gmail.com>
2016-09-07 09:39:37 -0700

    rearranged about me, and modified resume

--- public/src/components/aboutme.js
+++ public/src/components/aboutme.js
@@ -12,8 +12,8 @@ class AboutMe extends Component {
         <p>I am a software engineer that is passionate about building applications because I am able to break down a challenging goal into manageable steps.</p><br/>
         <p>I believe my applications are reflections of what I am as a software engineer. It starts from a humble beginning and with each iteration become better.</p><br/>
         <p>I built a web application called Hacker Habitat that models this idea. It started with a simple interface creating listings for homes and it grew into a full stack application with responsive maps and user messaging.</p><br/>
-        <p>I am also a California Registered Pharmacist with a strong knowledge of prescription medications and community based health care.</p><br/>
-        <p>I am currently looking for a full-time mid-level software engineering position or a per diem Pharmacist position.</p>
+        <p>I am also a California registered pharmacist with a strong knowledge of prescription medications and community-based health care.</p><br/>
+        <p>I am currently looking for a fulltime midlevel software engineering position or a per diem pharmacist position.</p>
       </div>
     );
   }
